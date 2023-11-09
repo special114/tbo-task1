1ca53768e7feebe2b07a46cfcaa4af038be44652
Rafał Surdej <special114@mailbox.org>
2023-11-09 21:38:38 +0100

    Added validation of input data in backend

--- Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
+++ Java/spring-thymeleaf-crud-example/src/main/java/com/example/thymeleaf/dto/CreateStudentDTO.java
@@ -4,9 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.*;
 import java.time.LocalDate;
 
 @Getter
@@ -14,6 +12,8 @@ import java.time.LocalDate;
 public class CreateStudentDTO {
 
     @NotEmpty(message = "{NotEmpty.name}")
+    @Size(min = 3, max = 400, message = "{Size.name}")
+    @Pattern(regexp = "[a-zA-Z- ]*", message = "{Pattern.name}")
     private String name;
 
     @Email
@@ -25,23 +25,36 @@ public class CreateStudentDTO {
     private LocalDate birthday;
 
     @NotEmpty(message = "{NotEmpty.zipCode}")
+    @Pattern(regexp = "^[0-9]{5}-[0-9]{3}$")
     private String zipCode;
 
     @NotEmpty(message = "{NotEmpty.street}")
+    @Size(min = 3, max = 400, message = "{Size.street}")
+    @Pattern(regexp = "[a-zA-Z- ]*", message = "{Pattern.street}")
     private String street;
 
     @NotEmpty(message = "{NotEmpty.number}")
+    @Size(min = 1, max = 5, message = "{Size.number}")
+    @Pattern(regexp = "[0-9]+", message = "{Pattern.number}")
     private String number;
 
+    @Size(min = 3, max = 800, message = "{Size.complement}")
+    @Pattern(regexp = "[a-zA-Z0-9- ]*", message = "{Pattern.complement}")
     private String complement;
 
     @NotEmpty(message = "{NotEmpty.district}")
+    @Size(min = 3, max = 400, message = "{Size.district}")
+    @Pattern(regexp = "[a-zA-Z- ]*", message = "{Pattern.district}")
     private String district;
 
     @NotEmpty(message = "{NotEmpty.city}")
+    @Size(min = 3, max = 400, message = "{Size.city}")
+    @Pattern(regexp = "[a-zA-Z- ]*", message = "{Pattern.city}")
     private String city;
 
     @NotEmpty(message = "{NotEmpty.state}")
+    @Size(min = 3, max = 400, message = "{Size.state}")
+    @Pattern(regexp = "[a-zA-Z- ]*", message = "{Pattern.state}")
     private String state;
 
 }
